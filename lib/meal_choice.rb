e125630072e7fa1deaea5fc00a156d63bfba8824
Tagir Galiyev <w.tagir.galiyev@gmail.com>
2020-05-12 21:12:01 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -6,4 +6,5 @@ def meal_choice(veg1, veg2, protein = "tofu")
   p "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
-meal_choice("broccoli", "macaroni", "roast beef")
+meal = meal_choice("broccoli", "macaroni", "roast beef")
+puts meal
